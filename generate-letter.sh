ff8e51e9f71b5feccad829fb8733dcc2679d589e
albert <albert.suarez.molgo@est.fib.upc.edu>
2017-09-09 19:12:21 +0200

    change template and add script

---
+++ generate-letter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+NAME="$1"
+ADDRESS="$2"
+POSITION="$3"
+
+cp main.tex aux1.tex
+sed -i "s/Biene Company/$NAME/g" aux1.tex
+sed -i "s/Bienilandia/$ADDRESS/g" aux1.tex
+sed -i "s/Biene meme generator/$POSITION/g" aux1.tex
+pdflatex aux1.tex
+rm aux1.tex
+rm aux1.aux
+rm aux1.log
+rm aux1.out
+mv aux1.pdf cover-letters/"Albert Suarez cover letter - $NAME.pdf"
