cd757df1cbec4f17df8a668215419ffd6aba00a7
albert <albert.suarez.molgo@est.fib.upc.edu>
2017-09-09 23:44:20 +0200

    change company script

--- generate-letter-company.sh
+++ generate-letter-company.sh
@@ -12,4 +12,4 @@ rm aux1.tex
 rm aux1.aux
 rm aux1.log
 rm aux1.out
-mv aux1.pdf cover-letters/"Albert Suarez cover letter - $NAME.pdf"
+mv aux1.pdf cover-letters/"Albert Suarez cover letter - $NAME - $POSITION.pdf"
